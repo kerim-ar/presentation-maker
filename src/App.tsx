d31f5e8ecdc6e61578f937e6586ed81237986444
Kerim Arnaberdiev <kerim.arnaberdiev@ispring.com>
2024-10-14 12:54:54 +0300

    Добавил базовый лайаут приложения. Добавил

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
+import {SlidesList} from "./view/SlidesList.tsx";
+import {TopPanel} from "./view/topPanel/TopPanel.tsx";
+import {Workspace} from "./view/Workspace.tsx";
+import {EditorType} from "./store/EditorType.ts";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type AppProps = {
+    editor: EditorType,
+}
+function App({editor}: AppProps) {
+    return (
+        <>
+            <TopPanel title={editor.presentation.title}></TopPanel>
+            <div className={styles.container}>
+                <SlidesList slides={editor.presentation.slides} selection={editor.selection}></SlidesList>
+                <Workspace slide={editor.presentation.slides[0]}></Workspace>
+            </div>
+        </>
+    )
 }
 
 export default App
